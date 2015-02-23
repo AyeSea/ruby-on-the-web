22a1998bac7ca7b9e7b3faa3e280b9e54f11fa20
Andrew Chung <andrew.chung159@gmail.com>
2015-02-22 21:46:45 -0500

    Refactored code to be more object-oriented. Adding POST functionality to browser and server.

--- browser_server/browser.rb
+++ browser_server/browser.rb
@@ -1,47 +1,59 @@
 require 'socket'
 require 'json'
 
-def request_type
-	loop do
-		puts "Please enter what type of request you want to send:"
-		puts "(options are GET and POST)"
-		reply = gets.chomp.upcase
-		return reply if ["GET", "POST"].include?(reply)
+class Browser
+	def initialize(host = "localhost", port = 2000)
+		@host = host
+		@port = port
+		@socket = TCPSocket.open(host, port)
+	end
+
+	def run
+		create_http_req
+		send_req
+		receive_resp
+	end
+
+	private
+	def create_http_req
+		@req_type = confirm_req_type
+		@req_path = confirm_req_path
+		@content_type = @req_type == "GET" ? "text/html" : "application/x-www-form-urlencoded"
+		@req = "#{@req_type} #{@req_path} HTTP/1.0\r\n\r\n"
+	end
+
+	def send_req
+		@socket.print @req
+		headers
+	end
+
+	def receive_resp
+		resp = @socket.read
+		headers, body = resp.split("\r\n\r\n")
+		puts headers
+		puts body
+	end
+
+	def confirm_req_type
+		loop do
+			puts "Do you want to send your request as (GET) or (POST)?"
+			reply = gets.chomp.upcase
+			return reply if reply == "GET" || reply == "POST"
+			puts "#{reply} is not a valid response. Please try again."
+		end
+	end
+
+	def confirm_req_path
+		puts "Please enter the path of the file you want from #{@host}:"
+		reply = gets.chomp
+	end
+
+	def headers
+		@socket.puts "From: testuser@theodinproject.com"
+		@socket.puts "User-Agent: CLI Browser v1.0\r\n\r\n"
 	end
 
 end
 
-def post_details
-	form_data = {}
-	form_data["viking"] = nil
-	puts "Please enter your viking's name:"
-	name = gets.chomp.upcase
-	puts "Please enter your email:"
-	email = gets.chomp
-	form_data[:viking] = {:name=>name, :email=>email}
-	form_data = form_data.to_json
-end
-
-host = "localhost"
-port = 2000
-req_type = request_type
-path = req_type == "GET" ? "/index.html" : "/thanks.html"
-content_type = path == "/index.html" ? "text/html" : "application/x-www-form-urlencoded"
-post_data = post_details if req_type == "POST"
-request = "#{req_type} #{path} HTTP/1.0\r\n\r\n"
-
-socket = TCPSocket.open(host, port)
-socket.puts(request)
-socket.puts "From: user@theodinproject.com"
-socket.puts "User-Agent: CLI Browser 1.0"
-socket.puts "Content-Type: #{content_type}"
-
-if post_data
-	socket.puts "Content-Length: #{post_data.length}"
-	socket.puts post_data
-end
-
-response = socket.read
-headers, body = response.split("\r\n\r\n", 2)
-puts headers
-puts body
+cli_browser = Browser.new
+cli_browser.run
--- browser_server/server.rb
+++ browser_server/server.rb
@@ -1,51 +1,81 @@
 require 'socket'
 require 'json'
 
-def display_headers(method, path, http_version, file)
-	#move header display logic over here. reuse for both GET and POST.
-end
-
-
-
-#Create a TCP/IP server socket to listen on port 2000.
-#Inherits from class IO, so same IO#open method that we use w/ the File class.
-server = TCPServer.open(2000)
-#Servers run forever so loop all actions.
-loop {
-	#Waits for a connection from a client, then returns the TCPSocket object
-	#representing that connection (so client here is the client socket
-	#while server is the server socket).
-	client = server.accept
-	req = client.gets.split(" ")
-	method = req[0]
-	path = req[1]
-	http_version = req[2].chomp
-	file = path.gsub(/\//, "")
-
-	#If request method is GET and the path is "index.html", then
-	if method == "GET" && path == "/index.html"
-		#send properly formatted HTTP response to client and
-		client.puts "#{http_version} 200 OK"
-		client.puts "Date: #{File.mtime(file)}"
-		client.puts "Content-Type: #{File.extname(file)}"
-		client.puts "Content-Length: #{File.size(file)}"
-		puts "\r\n\r\n"
-		#open index.html and send its contents over to the client.
-		page_contents = File.readlines(file)
-
-		page_contents.each do |line|
-			client.puts line
-		end
-	elsif method == "POST" && path == "/thanks.html"
-		#display 
-	else
-		client.puts "#{http_version} 404 Not Found"
+class Server
+	def initialize(port = 2000)
+		@server = TCPServer.open(port)
 	end
 
-	#Send time to the client
-	client.puts(Time.now.ctime)
-	#Sends display of string to the client
-	client.puts "Closing the connection. Bye!"
-	#Disconnects from the client socket and closes the connection.
-	client.close
-}
+	def run
+		loop {
+			@client = @server.accept
+			receive_from_client
+			send_to_client
+			@client.puts "Closing the connection. Bye!"
+			@client.close
+		}
+	end
+
+	def receive_from_client
+		#update line below so that the connection doesn't close....
+		#the read method closes the connection once all contents are read in
+		@req = @client.gets.split(" ")
+		@req_type = @req[0]
+		@req_path = @req[1]
+		puts @req.join(" ")
+
+		@headers = [] 
+		3.times { @headers << @client.gets }
+		puts @headers
+	end
+
+	def send_to_client
+		@req_type == "GET" ? for_get : for_post
+	end
+
+	def for_get
+		if @req_path == "/index.html"
+			send_response_line
+			file = "index.html"
+			send_headers(file)
+			
+			page_contents = File.readlines(file)
+
+			page_contents.each do |line|
+				@client.puts line
+			end
+		else
+			send_error_msg
+		end
+	end
+
+	def for_post
+		if @req_path == "/thanks.html"
+			send_response_line
+			file = "thanks.html"
+			send_headers(file)
+			###
+			###CODE FOR SENDING THANKS.HTML BODY OVER HERE!!!
+			###
+		else
+			send_error_msg
+		end
+	end
+
+	def send_response_line
+		@client.puts "HTTP/1.0 200 OK"
+	end
+
+	def send_headers(file)
+		@client.puts "Date: #{File.mtime(file)}"
+		@client.puts "Content-Type: #{File.extname(file)}"
+		@client.puts "Content-Length: #{File.size(file)}\r\n\r\n"
+	end
+
+	def send_error_msg
+		@client.puts "HTTP/1.0 404 File Not Found for #{@req_path}"
+	end
+end
+
+cli_server = Server.new
+cli_server.run
